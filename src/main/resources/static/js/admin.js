525d344a5a7bfe1686da8f82cbe6f921067b8b6a
ievgiienko@alliedbits.com <ievgiienko@alliedbits.com>
2018-04-13 01:29:32 +0300

    up4

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -3,14 +3,31 @@ $(document).ready(function(){
         var pageCount = (data.count / data.pageSize) + (data.count % data.pageSize > 0 ? 1 : 0);
         var i;
 
-        console.log(pageCount);
-
-        for (i = 0; i < pageCount; i++) {
-            var id = "page"+i;
+        for (i = 1; i <= pageCount; i++) {
+            var id = "page" + i;
 
             $('#pages').append(
                 $('<li>').attr('class', 'page-item').append(
-                    $('<a>').attr('class', 'page-link').attr('id', id).append(i)));
+                    $('<a>').attr('class', 'page-link').attr('id', id).append(i))
+            );
         }
     });
+
+    loadData(0);
 });
+
+function loadData(page) {
+    $.getJSON('/admin/geo?page=' + page, function(data) {
+        var i;
+
+        for (i = 0; i < data.length; i++) {
+            $('#data > tbody:last-child').append(
+                $('<tr>')
+                    .append($('<td>').append(data[i].ip))
+                    .append($('<td>').append(data[i].city))
+                    .append($('<td>').append(data[i].region))
+                    .append($('<td>').append(data[i].country))
+            );
+        }
+    });
+}
