c05b285ba4c4c40b131823cfb20a8d9ed54402e0
ievgiienko@alliedbits.com <ievgiienko@alliedbits.com>
2018-04-13 00:50:11 +0300

    up4

---
+++ src/main/java/ua/kiev/prog/AppConfig.java
@@ -0,0 +1,15 @@
+package ua.kiev.prog;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/admin").setViewName("redirect:/admin/");
+        registry.addViewController("/admin/").setViewName("forward:/admin/index.html");
+    }
+}
