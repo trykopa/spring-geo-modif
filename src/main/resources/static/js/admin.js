87a8881acab320e3efafa000b9cc65dba50e728d
ievgiienko@alliedbits.com <ievgiienko@alliedbits.com>
2018-04-13 01:13:54 +0300

    up4

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
         var pageCount = (data.count / data.pageSize) + (data.count % data.pageSize > 0 ? 1 : 0);
         var i;
 
+        console.log(pageCount);
+
         for (i = 0; i < pageCount; i++) {
             var id = "page"+i;
 
-            $('#content ul').append(
+            $('#pages').append(
                 $('<li>').attr('class', 'page-item').append(
-                    $('<a>').attr('class', 'page-link').attr('id',id).append(
-                        i
-                    )));
+                    $('<a>').attr('class', 'page-link').attr('id', id).append(i)));
         }
     });
 });
